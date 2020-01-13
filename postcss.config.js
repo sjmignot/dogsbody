cafedf4109aaf44d00351cf279400672004ef45e
sjmignot <sjmignot@gmail.com>
2020-01-13 21:18:39 +0100

    optimize image size

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project
+  content: [
+    './templates/*.html',
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
@@ -5,5 +16,6 @@ module.exports = {
     require('cssnano')({
       preset: 'default',
     }),
+    ...[purgecss],
   ]
 }
