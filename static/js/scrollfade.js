1b7f9ba141d31760b17ea582539df1da54660988
sjmignot <sjmignot@gmail.com>
2020-01-23 11:21:15 +0100

    added simple dates and fade for dates

---
+++ static/js/scrollfade.js
@@ -0,0 +1,24 @@
+$(window).scroll(function() {
+  // Setting: Start fading halfway up the page
+  var startPos = 0.5;
+
+  // Cache window object
+  var $w = $(window);
+
+  // Basically, we go through each element and check its relative position within the viewport
+  $('.scroll-fade').each(function() {
+
+    // Get current relative position in viewport, based on the top edge
+    var pos = $(this).offset().top - $w.scrollTop();
+
+    // Get viewport height
+    var vh = $w.height();
+
+    if (pos < vh * startPos) {
+      // If element has past the starting threshold, we fade it
+      $(this).css('opacity', pos / (vh * startPos) * 1);
+    } else {
+      $(this).css('opacity', 1);
+    }
+  });
+});
