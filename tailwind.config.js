1bdbcce54d6690d7044e3085f919185abb6c7913
sjmignot <sjmignot@gmail.com>
2020-02-07 11:49:02 +0100

    update landing page for dogsbody

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 module.exports = {
   theme: {
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    },
     extend: {
       colors: {
         green: '#0F493F',
         red: '#C73E1D',
         orange: '#F18F01',
-        blue: '#0000EE'
       },
       spacing: {
         '27': '7.27rem',
