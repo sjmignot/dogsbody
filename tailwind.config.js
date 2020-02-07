89e23f0d6a4bb1036acfaffcc182dcd5b2dd31af
sjmignot <sjmignot@gmail.com>
2020-02-07 21:25:45 +0100

    getting closer to fixing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   theme: {
+    screens: {
+      xs: '375px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     fontSize: {
       'xs': '.75rem',
       'sm': '.875rem',
